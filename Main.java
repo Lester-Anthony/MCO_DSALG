816327fdbf875e698357e11465d8e50453aa0588
LesterAnthony <lesteranthonyjr@gmail.com>
2023-06-25 00:22:35 +0800

    Initial functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: Use this method to run your experiments.
+        SortingAlgorithms sortingData = new SortingAlgorithms();
+        FileReader fileReaderObject = new FileReader();
+        Record[] recordData = fileReaderObject.readFile("data/random25000.txt");
+        int n = 0;
+        long start, end, time;
+
+        try {
+            File f = new File("data/random25000.txt");
+            Scanner scanner = new Scanner(f);
+            n = scanner.nextInt();
+            scanner.close();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        start = System.currentTimeMillis();
+        sortingData.insertionSort(recordData, n);
+        end = System.currentTimeMillis();
+        time = end - start;
+        System.out.println("Elapsed time: " + time + " milliseconds");
+
+        start = System.currentTimeMillis();
+        sortingData.selectionSort(recordData, n);
+        end = System.currentTimeMillis();
+        time = end - start;
+        System.out.println("Elapsed time: " + time + " milliseconds");
+
+        start = System.currentTimeMillis();
+        sortingData.mergeSort(recordData, 0, n - 1);
+        end = System.currentTimeMillis();
+        time = end - start;
+        System.out.println("Elapsed time: " + time + " milliseconds");
+
+        start = System.currentTimeMillis();
+        sortingData.bubbleSort(recordData, n);
+        end = System.currentTimeMillis();
+        time = end - start;
+        System.out.println("Elapsed time: " + time + " milliseconds");
     }
 }
--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -12,12 +12,27 @@ public class SortingAlgorithms {
 
     public void insertionSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
-
+        Record[] sortedArr = new Record[n];
+        int i, key, j;
+        for (i = 1; i < n; i++) {
+            key = arr[i].getIdNumber();
+            j = i - 1;
+    
+            // Move elements of arr[0..i-1],
+            // that are greater than key,
+            // to one position ahead of their
+            // current position
+            while (j >= 0 && arr[j].getIdNumber() > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            sortedArr[j + 1].getIdNumber = key;
+        }
     }
 
     public void selectionSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
-
+        
     }
 
     public void mergeSort(Record[] arr, int p, int r) {
@@ -30,5 +45,8 @@ public class SortingAlgorithms {
      * ones given above. Make sure that the method accepts an array of
      * records
      */
+    public void bubbleSort(Record[] arr, int p, int r) {
+
+    }
 
 }
