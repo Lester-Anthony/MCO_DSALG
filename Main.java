7b3dbb209be7152fbdc4cd0f1e4b29e4a4c3a227
LesterAnthony <lesteranthonyjr@gmail.com>
2023-06-25 02:58:58 +0800

    Datasets and Elapsed Time features implemented

--- Main.java
+++ Main.java
@@ -4,45 +4,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SortingAlgorithms sortingData = new SortingAlgorithms();
-        FileReader fileReaderObject = new FileReader();
-        Record[] recordData = fileReaderObject.readFile("data/random25000.txt");
-        int n = 0;
-        long start, end, time;
+        String[] filePath = {"data/almostsorted.txt", "data/random100.txt",
+                             "data/random25000.txt", "data/random50000.txt",
+                             "data/random75000.txt", "data/random100000.txt",
+                             "data/totallyreversed.txt"};
 
-        try {
-            File f = new File("data/random25000.txt");
-            Scanner scanner = new Scanner(f);
-            n = scanner.nextInt();
-            scanner.close();
+        for(String path : filePath) {
+
+            SortingAlgorithms sortingData = new SortingAlgorithms();
+            FileReader fileReaderObject = new FileReader();
+            Record[] recordData = fileReaderObject.readFile(path);
+            int n = 0;
+            long start, end, time;
+    
+            try {
+                File f = new File(path);
+                Scanner scanner = new Scanner(f);
+                n = scanner.nextInt();
+                scanner.close();
+            }
+    
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(path);
+    
+            start = System.currentTimeMillis();
+            sortingData.insertionSort(recordData, n);
+            end = System.currentTimeMillis();
+            time = end - start;
+            System.out.println("Insertion Sort");
+            System.out.println("Elapsed Time: " + time + " milliseconds\n");
+            
+            start = System.currentTimeMillis();
+            sortingData.selectionSort(recordData, n);
+            end = System.currentTimeMillis();
+            time = end - start;
+            System.out.println("Selection Sort");
+            System.out.println("Elapsed Time: " + time + " milliseconds\n");
+            
+            start = System.currentTimeMillis();
+            sortingData.mergeSort(recordData, 0, n - 1);
+            end = System.currentTimeMillis();
+            time = end - start;
+            System.out.println("Merge Sort");
+            System.out.println("Elapsed Time: " + time + " milliseconds\n");
+            
+            start = System.currentTimeMillis();
+            sortingData.bubbleSort(recordData, n);
+            end = System.currentTimeMillis();
+            time = end - start;
+            System.out.println("Bubble Sort");
+            System.out.println("Elapsed Time: " + time + " milliseconds\n");
         }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        start = System.currentTimeMillis();
-        sortingData.insertionSort(recordData, n);
-        end = System.currentTimeMillis();
-        time = end - start;
-        System.out.println("Elapsed time: " + time + " milliseconds");
-
-        start = System.currentTimeMillis();
-        sortingData.selectionSort(recordData, n);
-        end = System.currentTimeMillis();
-        time = end - start;
-        System.out.println("Elapsed time: " + time + " milliseconds");
-
-        start = System.currentTimeMillis();
-        sortingData.mergeSort(recordData, 0, n - 1);
-        end = System.currentTimeMillis();
-        time = end - start;
-        System.out.println("Elapsed time: " + time + " milliseconds");
-
-        start = System.currentTimeMillis();
-        sortingData.bubbleSort(recordData, n);
-        end = System.currentTimeMillis();
-        time = end - start;
-        System.out.println("Elapsed time: " + time + " milliseconds");
     }
 }
--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -84,7 +84,13 @@ public class SortingAlgorithms {
             k++;
         }
     }
-
+    
+    /*
+     * Define AT LEAST ONE more sorting algorithm here, apart from the
+     * ones given above. Make sure that the method accepts an array of
+     * records
+     */
+    
     public void bubbleSort(Record[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
@@ -96,11 +102,6 @@ public class SortingAlgorithms {
             }
         }
     }
-
-    /*
-     * Define AT LEAST ONE more sorting algorithm here, apart from the
-     * ones given above. Make sure that the method accepts an array of
-     * records
-     */
-
+    
+    
 }
