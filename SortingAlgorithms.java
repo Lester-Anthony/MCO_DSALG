a3cbfe8910f025fa40b8ee9d5cf1634088ebdfb5
LesterAnthony <lesteranthonyjr@gmail.com>
2023-06-26 01:21:07 +0800

    Final

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -11,65 +11,71 @@ public class SortingAlgorithms {
      */
 
     public void insertionSort(Record[] arr, int n) {
-        for (int i = 1; i < n; i++) {                                        
-            Record key = arr[i];                                                  
-            int j = i - 1;                                                             
-            while (j >= 0 && arr[j].getIdNumber() > key.getIdNumber()) {               
-                arr[j + 1] = arr[j];
-                j--;
+        for (int i = 1; i < n; i++) { // (n)
+            Record key = arr[i]; // n-1
+            int j = i - 1; // n-1
+            while (j >= 0 && arr[j].getIdNumber() > key.getIdNumber()) { // (n-1) (n+1)
+                arr[j + 1] = arr[j]; // (n-1) (n)
+                j--; // (n-1) (n)
             }
-            arr[j + 1] = key;
+            arr[j + 1] = key; // n-1
         }
     }
+
+    // (n) + (n-1) + (n-1) + (n-1) (n+1) + (n-1) (n) + (n-1) (n) + (n-1) = 3n^² + 4n - 3
 
     public void selectionSort(Record[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j].getIdNumber() < arr[minIndex].getIdNumber()) {
-                    minIndex = j;
+        for (int i = 0; i < n; i++) { // (n+1)
+            int minIndex = i; // (n)
+            for (int j = i + 1; j < n; j++) { // (n) (n+1)/2
+                if (arr[j].getIdNumber() < arr[minIndex].getIdNumber()) { // (n) (n+1)/2
+                    minIndex = j; // (n) (n+1)/2
                 }
             }
-            Record temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+            Record temp = arr[minIndex]; // (n)
+            arr[minIndex] = arr[i]; // (n)
+            arr[i] = temp; // (n)
         }
     }
+
+    // (n+1) + (n) + (n) (n+1)/2 + (n) (n+1)/2 + (n) (n+1)/2 + (n) + (n) + (n) = n^2 + 6n + 1
 
     public void mergeSort(Record[] arr, int p, int r) {
-        if (p < r) {
-            int q = (p + r) / 2;
-            mergeSort(arr, p, q);
-            mergeSort(arr, q + 1, r);
-            merge(arr, p, q, r);
+        if (p < r) { // 2n -1
+            int q = (p + r) / 2; // n-1
+            mergeSort(arr, p, q); // n-1
+            mergeSort(arr, q + 1, r); // n-1
+            merge(arr, p, q, r); // (7n + 11)
         }
     }
 
-    private void merge(Record[] arr, int p, int q, int r) {
-        int leftSide = q - p + 1;
-        int rightSide = r - q;
+    // T(n) = log2(n)*(12n + 7)
 
-        Record[] left = new Record[leftSide];
-        Record[] right = new Record[rightSide];
+    private void merge(Record[] arr, int p, int q, int r) { // (1)
+        int leftSide = q - p + 1; // (1)
+        int rightSide = r - q; // (1)
 
-        for (int i = 0; i < leftSide; i++) {
-            left[i] = arr[p + i];
+        Record[] left = new Record[leftSide]; // (1)
+        Record[] right = new Record[rightSide]; // (1)
+
+        for (int i = 0; i < leftSide; i++) { // (n/2+1)
+            left[i] = arr[p + i]; // (1)
         }
-        for (int j = 0; j < rightSide; j++) {
-            right[j] = arr[q + 1 + j];
+        for (int j = 0; j < rightSide; j++) { // (n/2+1)
+            right[j] = arr[q + 1 + j]; // (1)
         }
 
-        int i = 0, j = 0, k = p;
+        int i = 0, j = 0, k = p; // (3)
 
-        while (i < leftSide && j < rightSide) {
-            if (left[i].getIdNumber() <= right[j].getIdNumber()) {
-                arr[k] = left[i];
-                i++;
+        while (i < leftSide && j < rightSide) { // (n+1)
+            if (left[i].getIdNumber() <= right[j].getIdNumber()) { // (n) (1)
+                arr[k] = left[i]; // (n) (1)
+                i++; // (n) (1)
             } else {
                 arr[k] = right[j];
                 j++;
             }
-            k++;
+            k++; // (n) (1)
         }
 
         while (i < leftSide) {
@@ -85,23 +91,27 @@ public class SortingAlgorithms {
         }
     }
     
+    // 1 + 1 + 1 + 1 + 1 + (n/2+1) + 1 + (n/2+1) + 1 + 3 + (n+1) + 4n = 7n + 11
+
+    public void bubbleSort(Record[] arr, int n) {
+        for (int i = 0; i < n; i++) { // (n+1)
+            for (int j = 0; j < n - i - 1; j++) { // (n) (n+1)/2
+                if (arr[j].getIdNumber() > arr[j + 1].getIdNumber()) { // (n) (n+1)/2
+                    Record temp = arr[j]; // (n) (n+1)/2
+                    arr[j] = arr[j + 1]; // (n) (n+1)/2
+                    arr[j + 1] = temp; // (n) (n+1)/2
+                }
+            }
+        }
+    }
+
+    // (n+1) + (n) (n+1)/2 + (n) (n+1)/2 + (n) (n+1)/2 + (n) (n+1)/2 = 2n^2 + 3n + 1
+
     /*
      * Define AT LEAST ONE more sorting algorithm here, apart from the
      * ones given above. Make sure that the method accepts an array of
      * records
      */
-    
-    public void bubbleSort(Record[] arr, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j].getIdNumber() > arr[j + 1].getIdNumber()) {
-                    Record temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
     
     public boolean isSorted(Record[] arr) {
         int n = arr.length;
@@ -112,4 +122,5 @@ public class SortingAlgorithms {
         }
         return true;
     }
+
 }
